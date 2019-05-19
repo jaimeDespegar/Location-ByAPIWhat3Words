02e6a9b34b2eda7aa8942820a43663a2f6cf0363
jaimeDespegar <jaime.quispe@despegar.com>
2019-05-19 10:49:11 -0300

    creo la base

---
+++ src/main/java/api/WhatThreeWordsConnector.java
@@ -0,0 +1,29 @@
+package api;
+
+import model.Coordinate;
+import model.LocationConnector;
+import java.util.List;
+
+public class WhatThreeWordsConnector implements LocationConnector {
+
+    @Override
+    public Boolean isAvailable() {
+        return null;
+    }
+
+    @Override
+    public Boolean on() {
+        return null;
+    }
+
+    @Override
+    public Boolean off() {
+        return null;
+    }
+
+    @Override
+    public List<Coordinate> getData(Coordinate arrival, Coordinate departure) {
+        return null;
+    }
+
+}
